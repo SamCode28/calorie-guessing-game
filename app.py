acbb9431798506218406d029cb25ea6e3b1d3bca
SamCode28 <sglowe28@gmail.com>
2025-06-25 10:03:56 -0500

    added port and host

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from requests_oauthlib import OAuth1Session
+import os
 
 app = Flask(__name__)
 
@@ -23,4 +24,5 @@ def search_food():
     return jsonify(resp.json())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
