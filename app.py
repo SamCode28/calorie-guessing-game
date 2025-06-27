e7e994d31f79373b07677f074c66bd4a699026bb
SamCode28 <sglowe28@gmail.com>
2025-06-27 11:18:04 -0500

    added HTTPBasicAuth

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
-from requests_oauthlib import OAuth1Session
 import os
 import requests
+from requests.auth import HTTPBasicAuth
 
 app = Flask(__name__)
 
@@ -17,41 +17,25 @@ def index():
 def test_func():
     return render_template("index.html")
 
-@app.route('/get-token')
+@app.route('/get-token', methods=['POST'])
 def get_token():
     token_url = 'https://oauth.fatsecret.com/connect/token'
     data = {
         'grant_type': 'client_credentials',
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'scope' : 'premiere'
+        'scope' : 'premier'
     }
 
-    response = requests.post(token_url, data=data)
+    response = requests.post(
+        token_url,
+        data=data,
+        auth=HTTPBasicAuth(client_id, client_secret)
+    )
 
     if response.status_code == 200:
         token_info = response.json()
         return jsonify(token_info)
     else:
         return jsonify({'error': 'Failed to get token', 'details': response.text}), response.status_code
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-@app.route('/api/search-food')
-def search_food():
-    query = request.args.get('query', '')
-    oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
-
-    url = 'https://platform.fatsecret.com/rest/server.api'
-    params = {
-        'method': 'foods.search',
-        'format': 'json',
-        'search_expression': query
-    }
-
-    resp = oauth.get(url, params=params)
-    return jsonify(resp.json())
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
