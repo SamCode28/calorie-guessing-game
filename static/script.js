534cd5acf455ad06a3eef89eeb9bf1bc9b245d17
SamCode28 <sglowe28@gmail.com>
2025-07-02 08:19:47 -0500

    reduced redundancy in fetch calls

--- static/script.js
+++ static/script.js
@@ -1,17 +1,12 @@
 fatSecretHeader = new Headers();
 fatSecretHeader.append("Content-Type", "application/json")
 
-document.getElementById('play-game-1').addEventListener('click', () => {
-  fetch('/get-token', {method: 'POST',})
-});
-
-
 document.getElementById('play-game-2').addEventListener('click', () => {
   fetch('/get-food', {
   method: 'GET',
   headers: fatSecretHeader})
   .then(response => response.text())
-  .then(response_text => document.getElementById('play-game-1').innerText=response_text)
+  .then(response_text => document.getElementById('src-container').setAttribute("src",response_text))
   .catch(error => {
     console.error('API error:', error);
   });
