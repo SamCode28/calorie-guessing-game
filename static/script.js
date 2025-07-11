c98247c599fc30ebaf207c293ce7c0b5d9d37322
SamCode28 <sglowe28@gmail.com>
2025-07-11 11:54:01 -0500

    Rough CSS Outline

--- static/script.js
+++ static/script.js
@@ -1,14 +1,28 @@
 fatSecretHeader = new Headers();
 fatSecretHeader.append("Content-Type", "application/json")
 
-document.getElementById('play-game-2').addEventListener('click', () => {
+document.getElementById('play-game-1').addEventListener('click', () => {
   fetch('/get-food', {
   method: 'GET',
   headers: fatSecretHeader})
   .then(response => response.text())
-  .then(response_text => document.getElementById('src-container').setAttribute("src",response_text))
+  .then(response_text =>{
+    document.getElementById('img-container-1').setAttribute("src",response_text)
+    document.getElementById('img-container-2').setAttribute("src",response_text)
+    hideClass('main-menu-container')
+    removeHideClass("game-grid-container")
+    //removeHideClass('card-2')
+  })
   .catch(error => {
     console.error('API error:', error);
   });
 });
+
+function hideClass(id){
+  document.getElementById(id).classList.add('hidden')
+}
+
+function removeHideClass(id){
+  document.getElementById(id).classList.remove('hidden')
+}
 
