6ab1fa6c6063f93840bb6946c6e78e4a80548f08
SamCode28 <sglowe28@gmail.com>
2025-07-23 12:18:43 -0500

    updated get_food() to return all relevant food information

--- app.py
+++ app.py
@@ -41,42 +41,74 @@ def index():
 def get_food():
     token_url = 'https://platform.fatsecret.com/rest/foods/search/v3'
     headers = {
+        #required
         'Content-Type': 'application/json',
         'Authorization': f"Bearer {token_found.get("access_token")}"
     }
-    payload = {
+    params = {
+        #required
         'method' : 'foods.search.v3',
-        'search_expression' : "apple",
-        'include_food_images' : True,
-        'flag_default_serving' : True,
-        'format' : "json"
+        'format' : "json",
+        #optional
+        'search_expression' : "apple", #Placeholder
+        'include_food_images' : True,        
     }
 
-    response = requests.get(token_url, headers=headers, params=payload)
+    response = requests.get(token_url, headers=headers, params=params)
     response = response.json()
 
-    random_valid_food_dict_key = random.randint(0,len(response['foods_search']['results']['food']))
-
-    print(f'key: {random_valid_food_dict_key}')
-
-    attempts = len(response['foods_search']['results']['food'])
-
-    while(attempts > 1):
-        random_valid_food_dict_key = random.randint(0,len(response['foods_search']['results']['food']) - 1)
-        try:
-            if(response['foods_search']['results']['food'][random_valid_food_dict_key]['food_images']):
-                print('Link found')
-                print(f'Calories: {response['foods_search']['results']['food'][random_valid_food_dict_key]['servings']['serving'][0]['calories']}')
-                break
-        except KeyError:
-            print('No link found')
-            print(f"except #: {random_valid_food_dict_key}")
-            attempts -= 1
+    food_list = response['foods_search']['results']['food']
 
     try:
-        return response['foods_search']['results']['food'][random_valid_food_dict_key]['food_images']['food_image'][0]['image_url']
+        valid_food = return_valid_food_from_list(food_list)
     except KeyError:
-        return jsonify({'error': 'Invalid JSON from FatSecret', 'raw': response.text}), 502
+        pass
+        #Create error to retry with new search term
+
+    #Object to be sent back
+    food_data = {'food_name' : valid_food['food_name'],
+                 'img_url' : valid_food['food_images']['food_image'][0]['image_url'],
+                 'protein' : "",
+                 'carbs' : "",
+                 'fiber' : "",
+                 'fat' : "",
+                 'calories' : ""
+                 }
+
+    #Check to see if food contains data with 100g serving size
+    for serving in valid_food['servings']['serving']:
+        if serving['serving_description'] == "100 g":
+            food_data['protein'] = f'{serving['protein']} g'
+            food_data['carbs'] =  f'{serving['carbohydrate']} g'
+            food_data['fiber'] =  f'{serving['fiber']} g'
+            food_data['fat'] = f'{serving['fat']} g'
+            food_data['calories'] = f'{serving['calories']} kcal'
+            print(f"food:{food_data}")
+            return jsonify(food_data)
+    print("No match found")
+        #Create error if none found
+            
+    #saved for future use
+    #except KeyError:
+    #    return jsonify({'error': 'Invalid JSON from FatSecret', 'raw': response.text}), 502
+    
+
+def return_valid_food_from_list(food_dict):
+    num_reults = len(food_dict)
+    random_key = random.randint(0, num_reults - 1)
+    test_key = random_key + 1
+
+    while(random_key != test_key):
+        if (test_key == num_reults):
+                test_key = 0
+        if('food_images' in food_dict[test_key] and food_dict[test_key]['food_type'] == "Generic"):
+            return food_dict[test_key]
+        test_key += 1
+    
+    return KeyError
+            
+
+
     
 
 if __name__ == '__main__':
@@ -87,3 +119,61 @@ if __name__ == '__main__':
 #curl -X 'GET' \
 #  'https://api.nal.usda.gov/fdc/v1/food/2262074?format=abridged&nutrients=957&nutrients=203&nutrients=204&nutrients=205' \
 #  -H 'accept: application/json'
+
+#{
+#  "foods_search": {
+#    "max_results": "20",
+#    "total_results": "2004",
+#    "page_number": "0",
+#    "results": {
+#      "food": [
+#        {
+#          "food_id": "35718",
+#          "food_name": "Apples",
+#          "food_type": "Generic",
+#          "food_url": "https://foods.fatsecret.com/calories-nutrition/usda/apples",
+#          "food_images": {
+#            "food_image": [
+#              {
+#                "image_url": "https://www.foodimagedb.com/food-images/6b5d0828-7391-49e6-ba02-606b0363d41e_1024x1024.png",
+#                "image_type": "0"
+#              },
+#              {
+#                "image_url": "https://www.foodimagedb.com/food-images/6b5d0828-7391-49e6-ba02-606b0363d41e_400x400.png",
+#                "image_type": "0"
+#              },
+#              {
+#                "image_url": "https://www.foodimagedb.com/food-images/6b5d0828-7391-49e6-ba02-606b0363d41e_72x72.png",
+#                "image_type": "0"
+#              }
+#            ]
+#          },
+#          "servings": {
+#            "serving": [
+#              {
+#                "serving_id": "32915",
+#                "serving_description": "1 medium (2-3/4\" dia) (approx 3 per lb)",
+#                "serving_url": "https://foods.fatsecret.com/calories-nutrition/usda/apples?portionid=32915&portionamount=1.000",
+#                "metric_serving_amount": "138.000",
+#                "metric_serving_unit": "g",
+#                "number_of_units": "1.000",
+#                "measurement_description": "medium (2-3/4\" dia) (approx 3 per lb)",
+#                "is_default": "1",
+#                "calories": "72",
+#                "carbohydrate": "19.06",
+#                "protein": "0.36",
+#                "fat": "0.23",
+#                "saturated_fat": "0.039",
+#                "polyunsaturated_fat": "0.070",
+#                "monounsaturated_fat": "0.010",
+#                "cholesterol": "0",
+#                "sodium": "1",
+#                "potassium": "148",
+#                "fiber": "3.3",
+#                "sugar": "14.34",
+#                "vitamin_a": "4",
+#                "vitamin_c": "6.3",
+#                "calcium": "8",
+#                "iron": "0.17"
+#              },
+             
